0bce2cdc94a5860f8f67ac67cbba8691900757f8
McTalian <5081281+McTalian@users.noreply.github.com>
2026-04-27 14:12:36 -0600

    feat(unitframes): add position overrides for target-of-target and target-of-focus

--- BeaconUnitFrames/unitFrames/targetOfFocus/frame.lua
+++ BeaconUnitFrames/unitFrames/targetOfFocus/frame.lua
@@ -4,13 +4,13 @@ local ns = select(2, ...)
 ---@class BUFToFocus
 local BUFToFocus = ns.BUFToFocus
 
----@class BUFToFocus.Frame: Sizable, BackgroundTexturable
+---@class BUFToFocus.Frame: Sizable, BackgroundTexturable, FramePositionable
 local BUFToFocusFrame = {
 	configPath = "unitFrames.tofocus.frame",
 	frameKey = BUFToFocus.relativeToFrames.FRAME,
 }
 
-ns.Mixin(BUFToFocusFrame, ns.Sizable, ns.BackgroundTexturable)
+ns.Mixin(BUFToFocusFrame, ns.Sizable, ns.BackgroundTexturable, ns.FramePositionable)
 
 BUFToFocus.Frame = BUFToFocusFrame
 
@@ -24,6 +24,18 @@ ns.dbDefaults.profile.unitFrames.tofocus.frame = {
 	enableFrameTexture = true,
 	useBackgroundTexture = false,
 	backgroundTexture = "None",
+	enablePositionOverride = false,
+	anchorPoint = "TOPLEFT",
+	relativeTo = ns.Positionable.relativeToFrames.UI_PARENT,
+	relativePoint = "TOPLEFT",
+	xOffset = 0,
+	yOffset = 0,
+	frameStrata = "MEDIUM",
+	frameLevel = 0,
+	useBackdropBorder = false,
+	backdropBorderTexture = "None",
+	backdropEdgeSize = 16,
+	backdropBorderColor = { 1, 1, 1, 1 },
 }
 
 local frameOrder = {}
@@ -51,6 +63,7 @@ local frame = {
 
 ns.AddSizableOptions(frame.args, frameOrder)
 ns.AddBackgroundTextureOptions(frame.args, frameOrder)
+ns.AddFramePositionableOptions(frame.args, frameOrder)
 
 ns.options.args.tofocus.args.frame = frame
 
@@ -66,6 +79,20 @@ end
 function BUFToFocusFrame:RefreshConfig()
 	if not self.initialized then
 		BUFToFocus.FrameInit(self)
+		self.framePositionRelativeToOptions = {
+			[ns.Positionable.relativeToFrames.UI_PARENT] = ns.L["UIParent"],
+			[ns.Positionable.relativeToFrames.FOCUS_FRAME] = HUD_EDIT_MODE_FOCUS_FRAME_LABEL,
+			[ns.Positionable.relativeToFrames.TARGET_FRAME] = HUD_EDIT_MODE_TARGET_FRAME_LABEL,
+			[ns.Positionable.relativeToFrames.PLAYER_FRAME] = HUD_EDIT_MODE_PLAYER_FRAME_LABEL,
+			[ns.Positionable.relativeToFrames.PET_FRAME] = HUD_EDIT_MODE_PET_FRAME_LABEL,
+		}
+		self.framePositionRelativeToSorting = {
+			ns.Positionable.relativeToFrames.UI_PARENT,
+			ns.Positionable.relativeToFrames.FOCUS_FRAME,
+			ns.Positionable.relativeToFrames.TARGET_FRAME,
+			ns.Positionable.relativeToFrames.PLAYER_FRAME,
+			ns.Positionable.relativeToFrames.PET_FRAME,
+		}
 	end
 
 	if not self.frame then
@@ -84,6 +111,7 @@ function BUFToFocusFrame:RefreshConfig()
 	self:SetSize()
 	self:SetFrameTexture()
 	self:RefreshBackgroundTexture()
+	self:ApplyFramePosition()
 end
 
 function BUFToFocusFrame:SetSize()
@@ -129,7 +157,9 @@ end
 
 function BUFToFocusFrame:RefreshBackgroundTexture()
 	local useBackgroundTexture = self:DbGet("useBackgroundTexture")
-	if not useBackgroundTexture then
+	local useBackdropBorder = self:DbGet("useBackdropBorder")
+
+	if not useBackgroundTexture and not useBackdropBorder then
 		if self.backdropFrame then
 			self.backdropFrame:Hide()
 		end
@@ -141,22 +171,68 @@ function BUFToFocusFrame:RefreshBackgroundTexture()
 		self.backdropFrame:SetFrameStrata("BACKGROUND")
 	end
 
-	local backgroundTexture = self:DbGet("backgroundTexture")
-	local bgTexturePath = ns.lsm:Fetch(ns.lsm.MediaType.BACKGROUND, backgroundTexture)
-	if not bgTexturePath then
-		bgTexturePath = "Interface/None"
+	local bgTexturePath = nil
+	if useBackgroundTexture then
+		local backgroundTexture = self:DbGet("backgroundTexture")
+		bgTexturePath = ns.lsm:Fetch(ns.lsm.MediaType.BACKGROUND, backgroundTexture)
+		if not bgTexturePath then
+			bgTexturePath = "Interface/None"
+		end
 	end
 
-	self.backdropFrame:ClearAllPoints()
-	self.backdropFrame:SetAllPoints(BUFToFocus.frame)
+	local borderTexturePath = nil
+	if useBackdropBorder then
+		local backdropBorderTexture = self:DbGet("backdropBorderTexture")
+		borderTexturePath = ns.lsm:Fetch(ns.lsm.MediaType.BORDER, backdropBorderTexture)
+		if not borderTexturePath then
+			borderTexturePath = "Interface/Tooltips/UI-Tooltip-Border"
+		end
+	end
 
+	local backdropInsetLeft = self:GetBackdropInsetLeft()
+	local backdropInsetRight = self:GetBackdropInsetRight()
+	local backdropInsetTop = self:GetBackdropInsetTop()
+	local backdropInsetBottom = self:GetBackdropInsetBottom()
+
+	self.backdropFrame:ClearAllPoints()
+	self.backdropFrame:SetPoint("TOPLEFT", BUFToFocus.frame, "TOPLEFT", backdropInsetLeft, -backdropInsetTop)
+	self.backdropFrame:SetPoint(
+		"BOTTOMRIGHT",
+		BUFToFocus.frame,
+		"BOTTOMRIGHT",
+		-backdropInsetRight,
+		backdropInsetBottom
+	)
+
+	local backdropEdgeSize = self:DbGet("backdropEdgeSize")
 	self.backdropFrame:SetBackdrop({
 		bgFile = bgTexturePath,
-		edgeFile = nil,
+		edgeFile = borderTexturePath,
 		tile = true,
 		tileSize = 16,
-		edgeSize = 0,
+		edgeSize = useBackdropBorder and backdropEdgeSize or 0,
 		insets = { left = 0, right = 0, top = 0, bottom = 0 },
 	})
+
+	if useBackdropBorder then
+		local color = self:DbGet("backdropBorderColor")
+		self.backdropFrame:SetBackdropBorderColor(color[1], color[2], color[3], color[4])
+	end
+
 	self.backdropFrame:Show()
+end
+
+function BUFToFocusFrame:ApplyFramePosition()
+	if not self:GetEnablePositionOverride() then
+		return
+	end
+	self:_ApplyFramePosition(self.frame)
+end
+
+function BUFToFocusFrame:ResetFramePosition()
+	-- Blizzard repositions TargetOfFocus on updates; forcing a frame update
+	-- lets the default layout reassert itself.
+	if self.frame and self.frame.Update then
+		self.frame:Update()
+	end
 end
